e7ddb7f5dbc5a1bd923a0790678335aa48a0d944
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2026-07-22 17:34:59 +0300

    add redis support

--- NearbyPlaces/Controllers/NearbyPlacesController.cs
+++ NearbyPlaces/Controllers/NearbyPlacesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using NearbyPlaces.Data;
 using NearbyPlaces.Models;
+using NearbyPlaces.Redis;
 
 namespace NearbyPlaces.Controllers
 {
@@ -19,53 +20,25 @@ namespace NearbyPlaces.Controllers
         [HttpGet(Name = "GetAllPlaces")]
         public async Task<IEnumerable<Place>> GetAll()
         {
+            string res = await TestRedisGet();
             return await _db.Places.ToListAsync();
-        
-            /*
-                IList<Place> places = new List<Place>();
-                places.Add(new Place
-                {
-                    Name = "Jerusalem",
-                    City = "Jerusalem",
-                    Country = "Israel",
-                    Latitude = 31.7719,
-                    Longitude = 35.2170,
-                    Type = PlaceType.City
-                });
-                places.Add(new Place
-                {
-                    Name = "Aroma",
-                    City = "Jerusalem",
-                    Country = "Israel",
-                    Latitude = 31.7730,
-                    Longitude = 35.2180,
-                    Type = PlaceType.Cafe,
-                    Rating = 4.5
-                });
-                places.Add(new Place
-                {
-                    Name = "Paz Gas Station",
-                    City = "Jerusalem",
-                    Country = "Israel",
-                    Latitude = 31.7750,
-                    Longitude = 35.2200,
-                    Type = PlaceType.GasStation
-                });
-                return places;
-                */
         }
 
         [HttpPost]
         public async Task<IActionResult> Create(Place place)
         {
             _db.Places.Add(place);
-
             await _db.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAll), new { id = place.Id }, place);
+        }
 
-            return CreatedAtAction(
-                nameof(GetAll),
-                new { id = place.Id },
-                place);
+        public async Task<string> TestRedisGet()
+        {
+            RedisCrud redisCrud = new RedisCrud();
+            await redisCrud.Connect();
+            string res = await redisCrud.RetrieveValue("tr");
+            await redisCrud.Disonnect();
+            return res;
         }
     }
 }
---
+++ NearbyPlaces/Redis/RedisCrud.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+
+namespace NearbyPlaces.Redis
+{
+    public class RedisCrud
+    {
+        private ConnectionMultiplexer _connection;
+        IDatabase _db;
+        public async Task Connect()
+        {
+            string connectionString = "localhost:6379";
+            _connection = await ConnectionMultiplexer.ConnectAsync(connectionString);
+            _db = _connection.GetDatabase();
+        }
+
+        public async Task<string> RetrieveValue(string key)
+        {
+            return await _db.StringGetAsync(key);
+        }
+
+        public async Task Disonnect()
+        {
+            await _connection.CloseAsync();
+        }
+    }
+}
