e6b48a23dd87df25bb015e5a9881d55e6fa2db54
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2026-07-22 12:33:47 +0300

    start project

---
+++ NearbyPlaces/Controllers/NearbyPlacesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NearbyPlaces.Models;
+
+namespace NearbyPlaces.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NearbyPlacesController : Controller
+    {
+        [HttpGet(Name = "GetAllPlaces")]
+        public IEnumerable<Place> GetAll()
+        {
+            IList<Place> places = new List<Place>();
+            places.Add(new Place
+            {
+                Name = "Jerusalem",
+                City = "Jerusalem",
+                Country = "Israel",
+                Latitude = 31.7719,
+                Longitude = 35.2170,
+                Type = PlaceType.City
+            });
+            places.Add(new Place
+            {
+                Name = "Aroma",
+                City = "Jerusalem",
+                Country = "Israel",
+                Latitude = 31.7730,
+                Longitude = 35.2180,
+                Type = PlaceType.Cafe,
+                Rating = 4.5
+            });
+            places.Add(new Place
+            {
+                Name = "Paz Gas Station",
+                City = "Jerusalem",
+                Country = "Israel",
+                Latitude = 31.7750,
+                Longitude = 35.2200,
+                Type = PlaceType.GasStation
+            });
+            return places;
+        }
+    }
+}
---
+++ NearbyPlaces/Models/PlaceType.cs
@@ -0,0 +1,58 @@
+﻿namespace NearbyPlaces.Models;
+
+public enum PlaceType
+{
+
+    Unknown = 0,
+
+    // Geography
+    Country = 1,
+    City = 2,
+    Town = 3,
+    Village = 4,
+
+    // Food & Drink
+    Restaurant = 10,
+    Cafe = 11,
+    FastFood = 12,
+    Bakery = 13,
+    Bar = 14,
+
+    // Transportation
+    GasStation = 20,
+    Parking = 21,
+    EVChargingStation = 22,
+    Airport = 23,
+    TrainStation = 24,
+    BusStation = 25,
+
+    // Health
+    Hospital = 30,
+    Clinic = 31,
+    Pharmacy = 32,
+
+    // Finance
+    ATM = 40,
+    Bank = 41,
+
+    // Shopping
+    Supermarket = 50,
+    ShoppingMall = 51,
+    ConvenienceStore = 52,
+
+    // Tourism
+    Hotel = 60,
+    Museum = 61,
+    Beach = 62,
+    Park = 63,
+
+    // Government
+    PoliceStation = 70,
+    FireStation = 71,
+    PostOffice = 72,
+
+    // Education
+    School = 80,
+    University = 81
+
+}
