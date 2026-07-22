71cd72ae09c5cc23ee223095274d723dd876e1fb
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2026-07-22 12:34:11 +0300

    start project

---
+++ NearbyPlaces/Models/City.cs
@@ -0,0 +1,12 @@
+﻿namespace NearbyPlaces.Models;
+
+public class City
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = "";
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+}
---
+++ NearbyPlaces/Models/Place.cs
@@ -0,0 +1,20 @@
+﻿namespace NearbyPlaces.Models;
+
+public class Place
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string City { get; set; } = string.Empty;
+
+    public string Country { get; set; } = string.Empty;
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
+    public PlaceType Type { get; set; }
+
+    public double Rating { get; set; }
+}
